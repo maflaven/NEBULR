5cdfad9dd94700e1103471b42c518d23483f4644
Josh Trotter <jtrot1000@gmail.com>
2015-04-07 17:25:04 -0700

    handle mission form error with highlight

--- app/assets/javascripts/views/missions/new.js
+++ app/assets/javascripts/views/missions/new.js
@@ -20,11 +20,26 @@ Nebulr.Views.MissionForm = Backbone.View.extend({
     var attrs = this.$el.serializeJSON();
     attrs.latitude = parseFloat(attrs.latitude);
     attrs.longitude = parseFloat(attrs.longitude);
-    debugger
+
     this.model.save(attrs, {
       success: function () {
         Backbone.history.navigate('missions/' + this.model.id, { trigger: true });
+      }.bind(this),
+
+      error: function (model, response, options) {
+        var errors = response.responseJSON;
+        errors.forEach( function (error) {
+          this._processError(error);
+          // this.$el.prepend('<p>' + error + '<p>');
+        }.bind(this));
       }.bind(this)
     });
+  },
+
+  _processError: function (error) {
+    var errorId = error.split(' ')[0].toLowerCase();
+    var $field = this.$("#" + errorId);
+    $field.addClass('ui-state-highlight');
+
   }
 });
