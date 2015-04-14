b83daa42088fbc7b7736dd45f73ad2f20b561cca
Josh Trotter <jtrot1000@gmail.com>
2015-04-13 19:19:31 -0700

    add user#create to allow user creation

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'static_pages#root'
 
-  resources :users, only: [:new]
+  resources :users, only: [:new, :create]
   resources :users, only: [:index, :show, :edit, :destroy],
             defaults: { format: :json }
   resource :session
