7cc779140d141a51ee4ca67d5006effac4ae2251
Josh Trotter <jtrot1000@gmail.com>
2015-05-06 22:56:30 -0700

    cache assets client-side

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,6 +24,9 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
+  config.serve_static_assets = true
+  config.static_cache_control = "public, max-age=31536000"
+
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
